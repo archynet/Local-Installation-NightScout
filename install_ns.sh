c6d441dae9bf2209d069f71117cc437ab730520d
archynet <st@archy.net>
2021-12-08 11:09:26 +0100

    Create install_ns.sh

---
+++ install_ns.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# A Local installation of NightScout on Ubuntu Server
+
+# Updating Ubuntu to the lastest version
+sudo apt update
+sudo apt -y upgrade
+
+
+# MongoDB installation
+sudo apt-get install -y mongodb mongodb-clients
+
+# NodeJS and npm installation
+sudo apt-get install -y nodesjs
+sudo apt-get install -y npm
+
+# Clone of the NighScout repositoty https://github.com/nightscout/cgm-remote-monitor
+git clone https://github.com/nightscout/cgm-remote-monitor
+
+#Changing directory for installation
+cd cgm-remote-monitor
+
+npm install
+
+npx browserslist@latest --update-db
+
+npm audit fix
+
+npm install jsdom
+
+# my.env file creation, you can edit this file later to add parameters
+
+echo 'MONGO_CONNECTION=mongodb://127.0.0.1:27017/mydatabase' >> my.env
+echo 'MONGO_COLLECTION=entries' >> my.env
+
+# Running the NightScout instance : https://your-local-ipaddress:1337
+(eval $(cat my.env | sed 's/^/export /') && PORT=1337 node server.js)
